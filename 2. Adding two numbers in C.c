391c62164c26ba3e6201142ec0e841c47ca006bd
ShajeedAk <46027122+ShajeedAk@users.noreply.github.com>
2018-12-21 18:29:04 +0600

    Create 2. Adding two numbers in C.c

---
+++ 2. Adding two numbers in C.c
@@ -0,0 +1,15 @@
+// C Program for adding two numbers
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b,c;
+    a= 15;
+    b= 80;
+    c= a+b;
+    printf("Sum is %d\n",c);
+
+    return 0;
+}
